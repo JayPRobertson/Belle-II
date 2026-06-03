e8d4f30d90e884af1939bc9bf321e6d8e9e3eb30
JayPRobertson <162063771+JayPRobertson@users.noreply.github.com>
2026-06-03 13:25:59 -0700

    Updated run_exampleb2b.sh

--- Geant4/b2b/build/run_exampleb2b.sh
+++ Geant4/b2b/build/run_exampleb2b.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-FILEPATH = "$HOME/\"Belle II\"/Python/csv/hits"
+FILEPATH="$HOME/Desktop/Belle II"
 
-rm event_action_data.csv
-echo "event_id,num_trajectory,num_hits,energies,hitx,hity,hitz" > "event_action_data.csv"
+rm -f event_action_data.csv init_step_data.csv
+
+echo "event_id,num_trajectory,num_hits,energies,hitx,hity,hitz" > event_action_data.csv
+echo "energies,initx,inity,initz" > init_step_data.csv
 
 make
 ./exampleB2b
 
-cp event_action_data.csv "$FILEPATH"
+cp event_action_data.csv "$FILEPATH/Python/csv/hits/"
+cp init_step_data.csv "$FILEPATH/Python/csv/hits/"
